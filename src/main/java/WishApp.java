62f864eff563348e48ac37200e2ab981a85bb3a9
Shubhkant Dwivedi <shubhkant.dwivedi@hanu.com>
2022-11-29 11:04:18 +0530

    first commit

---
+++ src/main/java/WishApp.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class WishApp extends HttpServlet
+{
+     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+     {
+          
+     }
+}
